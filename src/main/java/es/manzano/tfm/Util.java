ba8fe4af4fc1369b214506e11e580f6293538a16
manzano <manzano@manzano-XPSM1330>
2019-07-17 00:38:27 +0200

    x

---
+++ src/main/java/es/manzano/tfm/Util.java
@@ -0,0 +1,23 @@
+package es.manzano.tfm;
+
+public class Util {
+
+	public static String extractPageNameFromURLString(String urlString){
+        if (urlString==null) return null;
+        int lastSlash = urlString.lastIndexOf("/");
+        //if (lastSlash==-1) lastSlash = 0;
+        String pageAndExtensions = urlString.substring(lastSlash+1);
+        int lastQuestion = pageAndExtensions.lastIndexOf("?");
+        if (lastQuestion==-1) lastQuestion = pageAndExtensions.length();
+        String result = pageAndExtensions.substring(0,lastQuestion);
+        
+        int lastpoint = result.lastIndexOf(".");
+        if (lastpoint!=-1) 
+        	{
+        	result=result.substring(0,lastpoint);
+        	}
+        return result;
+    }
+	
+	
+}
